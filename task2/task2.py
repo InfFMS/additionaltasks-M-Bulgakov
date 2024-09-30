a5a599c93cbafb43c360a386186f15bc3e7c20df
Tutor <m.bulgakov@school.nsu.ru>
2024-09-30 10:13:34 +0700

    3 задачи

--- task2/task2.py
+++ task2/task2.py
@@ -1,1 +1,11 @@
-
+s=[1,1,1]
+s[0] = int(input())
+s[1] = int(input())
+s[2] = int(input())
+x = int(max (s))
+y = int(min (s))
+m = 0
+for i in range(3):
+    if int(s[i]) < x and int(s[i]) > y:
+        m = s[i]
+print(m)
